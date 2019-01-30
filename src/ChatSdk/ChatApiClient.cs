c6198a36d91c977df6967139ec35ab20b3236b2e
SuxrobGM <suxrobGM@gmail.com>
2019-01-31 00:30:27 +0500

    added ChatSdk Api wrapper

---
+++ src/ChatSdk/ChatApiClient.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Http;
+
+namespace ChatSdk.Api
+{
+    public class ChatApiClient
+    {
+        private readonly HttpClient _request;
+        private readonly string _host;
+
+        public ChatApiClient()
+        {
+            _host = "localhost:44310";
+            _request = new HttpClient()
+            {
+                BaseAddress = new Uri($"https://{_host}/api/")
+            };
+        }
+
+
+    }
+}
