be10519b05876fc86a748b8ab9a5b3fb9dbe9d95
SuxrobGM <suxrobGM@gmail.com>
2019-01-31 22:23:45 +0500

    updated packages

--- src/ChatCore/Api/ChatApiClient.cs
+++ src/ChatCore/Api/ChatApiClient.cs
@@ -8,24 +8,34 @@ using Newtonsoft.Json.Linq;
 
 namespace ChatCore.Api
 {
+    public enum ApiVersion
+    {
+        v1,
+        v2
+    }
+
     public class ChatApiClient
     {
         private readonly HttpClient _request;
         private readonly string _host;
         private readonly string _token;
+        private ApiVersion _apiVersion;
 
-        public ChatApiClient()
+        public ApiVersion ApiVersion { get; set; }
+
+        public ChatApiClient(ApiVersion apiVersion = ApiVersion.v1)
         {
             _host = "localhost:44310";
+            _apiVersion = apiVersion;
             _request = new HttpClient()
             {
-                BaseAddress = new Uri($"https://{_host}/api/")
+                BaseAddress = new Uri($"https://{_host}/api/{_apiVersion}/")
             };
         }
 
         public async Task<bool> CheckUserExists(NetworkCredential networkCredential)
         {
-            var response = await _request.GetAsync($"/users?username={networkCredential.UserName}");
+            var response = await _request.GetAsync($"users?username={networkCredential.UserName}");
 
             if (response.IsSuccessStatusCode)
             {                
